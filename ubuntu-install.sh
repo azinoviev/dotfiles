484ccf96b01f5309003616cdf2eea89ae98c80e8
Alexey Zinoviev <alexey.zinoviev@gmail.com>
2014-05-16 12:17:13 +0400

    a few more packages to install list

--- ubuntu-install.sh
+++
@@ -1,5 +0,0 @@
-apt-get build-dep python python3 ipython vim emacs tmux llvm clang sbcl python-numpy python-scipy
-apt-get install zsh g++ valgrind gdb ddd tk-dev libdb-dev liblzma-dev build-essential zlib1g-dev libreadline-dev libncursesw5-dev libssl-dev libgdbm-dev sqlite3 libsqlite3-dev libbz2-dev htop atop ghc cmake curl wget git subversion mercurial dconf-cli freeglut3 freeglut3-dev binutils libgl1-mesa-dev libglc-dev freeglut3-dev libedit-dev libglw1-mesa libglw1-mesa-dev libjpeg-dev libgif-dev libtiff-dev libappindicator1 unrar libaio1 libmysqlclient18 libmysqlclient-dev libxslt-dev libcurl4-openssl-dev
-
-
-
